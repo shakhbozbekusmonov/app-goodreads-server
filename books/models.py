40aeb3a8a65ba3dbbfad5d65122b72445959f81c
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2022-10-29 17:19:49 +0500

    Add Admin Page Author, BookAuthor, BookReview and etc

--- books/models.py
+++ books/models.py
@@ -18,10 +18,16 @@ class Author(models.Model):
     email = models.EmailField()
     bio = models.TextField()
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class BookAuthor(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.book} by {self.author}"
 
 
 class BookReview(models.Model):
@@ -31,3 +37,6 @@ class BookReview(models.Model):
     starts_given = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
+
+    def __str__(self):
+        return f"{self.starts_given} starts for {self.book.title} by {self.user}"
